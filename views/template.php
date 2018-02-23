1a78e37158d6ea2e341f359156658617c446931d
Lucas Silva <luckkass89@gmail.com>
2018-02-23 14:50:37 -0300

    "Redimensionamento dos items no template"

--- views/template.php
+++ views/template.php
@@ -43,17 +43,22 @@
 		<div class="container_right">
 			<?php for($i = 0; $i< 15; $i++): ?>
 			<div class="item_right">
-				<?php for($j = 0; $j< 5; $j++): ?>
-				<div class="video_item">
-					<video class="video_item_frame" src="/assets/videos/videoplayback.mp4"></video>
+				
+				
+					<div class="slider_items">
+						<?php for($j = 0; $j< 5; $j++): ?>
+					<div class="video_item">
+					<a href="#"><video class="video_item_frame" src="/assets/videos/videoplayback.mp4"></video></a>
 					<div class="descricao">
+						<a href="#" class="titulo_video">Titulo asd saddasdasdasd asdasdasd</a><br>
 						<span>Autor</span> <br>
 						<span>Visualizações</span> <br>
-						<span>tempo</span>
 					</div>					
 				</div>
-
 				<?php endfor; ?>
+				</div>
+
+				
 			</div>
 		<?php endfor; ?>
 
