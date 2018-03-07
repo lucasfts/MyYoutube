ee8a079d1166ab217bfb5a96f051d8e69ccf9191
Lucas Silva <luckkass89@gmail.com>
2018-03-07 13:42:03 -0300

    Pagina users editadda

---
+++ controllers/userController.php
@@ -0,0 +1,14 @@
+<?php
+class userController extends Controller{
+
+	public function __construct(){
+		parent::__construct();
+	}
+	
+	public function index(){
+	
+		$dados = array();
+
+		$this->loadTemplate('user',$dados);
+	}
+}
---
+++ views/user.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" type="text/css" href="/assets/css/user.css">
+
+
+<div class="banner">
+	<img src="" width="100%" height="100%">
+</div>
+
+<div class="user_header">
+	<img src="/teste.jpg">
+	<div class="user_info">
+		<br>
+		<b style="font-size: 26px">Canal teste</b><br>
+		12312312 inscritos
+	</div>
+	<button>Inscreva-se</button>
+</div>
+
+<div class="container_videos">
+	<h1>Videos</h1>
+	<?php for($i = 0; $i < 30; $i++): ?>
+		<div class="video_item">
+			<video  src="/assets/videos/videoplayback.mp4" width="100%" height="150px" ></video>
+			<h5>Titulo do video</h5>
+			<p>123213213 Visualizações</p>
+			<p>123213 Likes  e 14123 DESLIKES</p>
+			<a href="">Editar</a>
+			<a href="">Excluir</a>
+		</div>
+
+	<?php endfor; ?>
+</div>
